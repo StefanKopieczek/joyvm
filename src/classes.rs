db62a17f4fc8e0c6dff7233d478d29c5afe9299d
Stefan Kopieczek <stefankopieczek@gmail.com>
2019-04-19 21:09:53 +0100

    Add support for Constants

--- src/classes.rs
+++ src/classes.rs
@@ -4,19 +4,57 @@ mod classes {
         pub major_version: u16,
         pub constants: Vec<Constant>,
         access_flags: u16,
-        pub this_class: ConstantRef,
-        pub super_class: ConstantRef,
-        pub interfaces: Vec<ConstantRef>,
+        pub this_class: ConstantIndex,
+        pub super_class: ConstantIndex,
+        pub interfaces: Vec<ConstantIndex>,
         pub fields: Vec<Field>,
         pub methods: Vec<Method>,
         pub attributes: Vec<Attribute>,
     }
 
-    pub struct ConstantRef(u16);
+    pub struct ConstantIndex(u16);
+    pub struct MethodIndex(u16);
 
     pub enum Constant {
-        Dummy,
+        Utf8(String),
+        Integer(u32),
+        Float(f32),
+        Long(u64),
+        Double(f64),
+        ClassRef(ConstantIndex),
+        StringRef(ConstantIndex),
+        FieldRef{class:ConstantIndex, name_and_type:ConstantIndex},
+        MethodRef{class:ConstantIndex, name_and_type:ConstantIndex},
+        InterfaceMethodRef{class:ConstantIndex, name_and_type:ConstantIndex},
+        NameAndTypeRef{name:ConstantIndex, descriptor:ConstantIndex},
+        MethodHandle(MethodHandleType),
+        MethodType(ConstantIndex),
+        InvokeDynamicInfo{bootstrap_method_attr:MethodIndex, name_and_type:ConstantIndex},
+        Dummy, // Necessary to fake Long and Double taking up two slots
     }
+
+    impl Constant {
+        pub fn get_tag(self) -> Option<u8> {
+            match self {
+                Constant::Utf8(_) => Some(1),
+                Constant::Integer(_) => Some(3),
+                Constant::Float(_) => Some(4),
+                Constant::Long(_) => Some(5),
+                Constant::Double(_) => Some(6),
+                Constant::ClassRef(_) => Some(7),
+                Constant::StringRef(_) => Some(8),
+                Constant::FieldRef{..} => Some(9),
+                Constant::MethodRef{..} => Some(10),
+                Constant::InterfaceMethodRef{..} => Some(11),
+                Constant::NameAndTypeRef{..} => Some(12),
+                Constant::MethodHandle(_) => Some(15),
+                Constant::MethodType(_) => Some(16),
+                Constant::InvokeDynamicInfo{..} => Some(18),
+                Constant::Dummy => None,
+            }
+        }
+    }
+
 
     pub struct Field {
     }
@@ -25,5 +63,17 @@ mod classes {
     }
 
     pub struct Attribute {
+    }
+
+    pub enum MethodHandleType {
+        GetField(ConstantIndex),
+        GetStatic(ConstantIndex),
+        PutField(ConstantIndex),
+        PutStatic(ConstantIndex),
+        InvokeVirtual(ConstantIndex),
+        InvokeStatic(ConstantIndex),
+        InvokeSpecial(ConstantIndex),
+        NewInvokeSpecial(ConstantIndex),
+        InvokeInterface(ConstantIndex),
     }
 }
