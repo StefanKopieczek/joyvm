e70be340e9dac2ea69b4434f17e49d4184593c24
Stefan Kopieczek <stefankopieczek@gmail.com>
2019-04-20 13:02:23 +0100

    Implement Field type

--- src/classes.rs
+++ src/classes.rs
@@ -57,12 +57,32 @@ mod classes {
 
 
     pub struct Field {
+        pub access_flags: FieldAccessFlags,
+        pub name: ConstantIndex,
+        pub descriptor: ConstantIndex,
+        pub attributes: Vec<Attribute>,
+    }
+
+    bitflags! {
+        pub struct FieldAccessFlags: u32 {
+            const PUBLIC    = 0x0001;
+            const PRIVATE   = 0x0002;
+            const PROTECTED = 0x0004;
+            const STATIC    = 0x0008;
+            const FINAL     = 0x0010;
+            const VOLATILE  = 0x0040;
+            const TRANSIENT = 0x0080;
+            const SYNTHETIC = 0x1000;
+            const ENUM      = 0x4000;
+        }
     }
 
     pub struct Method {
     }
 
     pub struct Attribute {
+        pub name: ConstantIndex,
+        pub contents: Vec<u8>,
     }
 
     pub enum MethodHandleType {
