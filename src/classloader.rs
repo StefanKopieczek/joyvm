9bf23b2e5430339a5c450ed076d4f1fda8522dc2
Stefan Kopieczek <stefankopieczek@gmail.com>
2019-04-21 12:04:53 +0100

    Implement float deserialization

--- src/classloader.rs
+++ src/classloader.rs
@@ -21,6 +21,7 @@ impl Deserialize for Constant {
         match tag {
             1 => deserialize_utf8(data),
             3 => deserialize_integer(data),
+            4 => deserialize_float(data),
             _ => Err(ClassLoaderError::InvalidConstantType(tag)),
         }
     }
@@ -50,6 +51,14 @@ fn deserialize_integer(data: &mut bytes::Buf) -> Result<Constant, ClassLoaderErr
     }
 
     return Ok(Constant::Integer(data.get_u32_be()));
+}
+
+fn deserialize_float(data: &mut bytes::Buf) -> Result<Constant, ClassLoaderError> {
+    if data.remaining() < 4 {
+        return Err(ClassLoaderError::Eof("Unexpected end of stream while parsing Float constant".to_string()));
+    }
+
+    return Ok(Constant::Float(data.get_f32_be()));
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -205,6 +214,122 @@ mod tests {
     #[test]
     fn test_deserialize_integer_premature_termination_4() {
         assert_eof_in_constant(b"\x03\xff\xff\xff");
+    }
+
+    #[test]
+    fn test_deserialize_float_smallest_possible_subnormal_number() {
+        do_float_test(0x00000001, b"\x04\x00\x00\x00\x01");
+    }
+
+    #[test]
+    fn test_deserialize_float_0x00000002() {
+        do_float_test(0x00000002, b"\x04\x00\x00\x00\x02");
+    }
+
+    #[test]
+    fn test_deserialize_float_largest_subnormal_number() {
+        do_float_test(0x007fffff, b"\x04\x00\x7f\xff\xff");
+    }
+
+    #[test]
+    fn test_deserialize_float_smallest_possible_normal_number() {
+        do_float_test(0x00800000, b"\x04\x00\x80\x00\x00");
+    }
+
+    #[test]
+    fn test_deserialize_float_largest_normal_number() {
+        do_float_test(0x7f7fffff, b"\x04\x7f\x7f\xff\xff");
+    }
+
+    #[test]
+    fn test_deserialize_float_largest_number_less_than_one() {
+        do_float_test(0x3f7fffff, b"\x04\x3f\x7f\xff\xff");
+    }
+
+    #[test]
+    fn test_deserialize_float_equal_to_one() {
+        do_float_test(0x3f800000, b"\x04\x3f\x80\x00\x00");
+    }
+
+    #[test]
+    fn test_deserialize_float_smallest_number_larger_than_one() {
+        do_float_test(0x3f800001, b"\x04\x3f\x80\x00\x01");
+    }
+
+    #[test]
+    fn test_deserialize_float_negative_two() {
+        do_float_test(0xc0000000, b"\x04\xc0\x00\x00\x00");
+    }
+
+    #[test]
+    fn test_deserialize_float_zero() {
+        do_float_test(0x00000000, b"\x04\x00\x00\x00\x00");
+    }
+
+    #[test]
+    fn test_deserialize_float_negative_zero() {
+        do_float_test(0x80000000, b"\x04\x80\x00\x00\x00");
+    }
+
+    #[test]
+    fn test_deserialize_float_positive_infinity() {
+        do_float_test(0x7f800000, b"\x04\x7f\x80\x00\x00");
+    }
+
+    #[test]
+    fn test_deserialize_float_pi() {
+        do_float_test(0x40490fdb, b"\x04\x40\x49\x0f\xdb");
+    }
+
+    #[test]
+    fn test_deserialize_float_one_third() {
+        do_float_test(0x3eaaaaab, b"\x04\x3e\xaa\xaa\xab");
+    }
+
+    #[test]
+    fn test_deserialize_float_qnan() {
+        // NaN != NaN so we have to check the result directly
+        let bytes: &[u8] = b"\x04\xff\xc0\x00\x01";
+        let result = Constant::deserialize(&mut bytes::Bytes::from(bytes).into_buf()).unwrap();
+        match result {
+            Constant::Float(ref float) => assert!(float.is_nan()),
+            _ => panic!("Expected float; got unexpected constant {:#?}", result),
+        }
+    }
+
+    #[test]
+    fn test_deserialize_float_snan() {
+        // NaN != NaN so we have to check the result directly
+        let bytes: &[u8] = b"\x04\xff\x80\x00\x01";
+        let result = Constant::deserialize(&mut bytes::Bytes::from(bytes).into_buf()).unwrap();
+        match result {
+            Constant::Float(ref float) => assert!(float.is_nan()),
+            _ => panic!("Expected float; got unexpected constant {:#?}", result),
+        }
+    }
+
+    #[test]
+    fn test_deserialize_float_premature_termination_1() {
+        assert_eof_in_constant(b"\x04");
+    }
+
+    #[test]
+    fn test_deserialize_float_premature_termination_2() {
+        assert_eof_in_constant(b"\x04\x00");
+    }
+
+    #[test]
+    fn test_deserialize_float_premature_termination_3() {
+        assert_eof_in_constant(b"\x04\x00\x00");
+    }
+
+    #[test]
+    fn test_deserialize_float_premature_termination_4() {
+        assert_eof_in_constant(b"\x04\x00\x00\x00");
+    }
+
+    fn do_float_test(float_bits: u32, input: &[u8]) {
+        assert_constant(Constant::Float(f32::from_bits(float_bits)), input);
     }
 
     fn assert_constant(constant: Constant, input: &[u8]) {
